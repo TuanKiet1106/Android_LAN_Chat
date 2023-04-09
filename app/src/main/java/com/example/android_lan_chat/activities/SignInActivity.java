842eb4b8ae3f4362becbf1393853df52d1215d0a
Kiet <20520906@gm.uit.edu.vn>
2023-04-09 13:49:15 +0700

    App Theme.

---
+++ app/src/main/java/com/example/android_lan_chat/activities/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.android_lan_chat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android_lan_chat.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
